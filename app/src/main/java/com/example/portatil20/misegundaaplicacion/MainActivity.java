1bfbad1315a9401544976b3c2798ea6e21d428d9
gatogoku <aqv1793@gmail.com>
2016-09-20 09:15:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/portatil20/misegundaaplicacion/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.portatil20.misegundaaplicacion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
